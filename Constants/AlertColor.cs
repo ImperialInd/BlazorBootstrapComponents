6b71a1e37031ef6cd23a883798c2bb0802d24251
Daniel Lutzow <31216511+ImperialInd@users.noreply.github.com>
2022-07-22 08:44:01 -0500

    Add project files.

---
+++ Constants/AlertColor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBootstrapComponents.Constants
+{
+	public enum AlertColor
+	{
+		Primary,
+		Secondary,
+		Success,
+		Danger,
+		Warning,
+		Info,
+		Light,
+		Dark
+	}
+}
