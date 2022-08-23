c2f85fcc5e1164d794e9348dfe2f44097a8160f5
tlutzow <95424926+tlutzow@users.noreply.github.com>
2022-08-23 09:36:36 -0500

    added image component

---
+++ Components/IIImages.razor.cs
@@ -0,0 +1,23 @@
+﻿using BlazorBootstrapComponents.Enums;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorBootstrapComponents.Components;
+
+public partial class IIImages
+{
+	protected override void OnInitialized()
+	{
+		if (Src.Length == 0) throw new Exception("A source must be supplied!");
+		base.OnInitialized();
+	}
+	[Parameter] public string Class { get; set; } = string.Empty;
+	[Parameter] public TextAlignmentEnum Alignment { get; set; } = TextAlignmentEnum.Empty;
+	[Parameter] public bool Responsive { get; set; }
+	[Parameter] public bool Thumbnail { get; set; }
+	[Parameter] public string Src { get; set; } = string.Empty;
+	[Parameter] public string Alt { get; set; } = string.Empty;
+
+
+	private Dictionary<string, object> AdditionalAttributes { get; set; } = new Dictionary<string, object>();
+
+}
