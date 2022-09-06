389b06068337a36e17ed9282071c4add6e0ae0bf
Daniel Lutzow <31216511+ImperialInd@users.noreply.github.com>
2022-09-06 11:21:27 -0500

    Update

--- Components/BSColumnLabel.razor.cs
+++ Components/BSColumnLabel.razor.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlazorBootstrapComponents.Components.Forms;
+namespace BlazorBootstrapComponents.Components;
 
 public partial class BSColumnLabel
 {
--- Components/BSRow.razor.cs
+++ Components/BSRow.razor.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlazorBootstrapComponents.Components.Forms;
+namespace BlazorBootstrapComponents.Components;
 
 public partial class BSRow
 {
