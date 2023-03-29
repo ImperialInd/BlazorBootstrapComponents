435f8e790554c168ff796efa67d1407971ed3b13
Daniel Lutzow <31216511+ImperialInd@users.noreply.github.com>
2023-03-29 15:12:13 -0500

    Update

--- Components/BSTextArea.razor.cs
+++ Components/BSTextArea.razor.cs
@@ -1,8 +1,11 @@
-﻿using BlazorBootstrapComponents.Enums;
+﻿using BaseClassLibrary.Classes;
+using BlazorBootstrapComponents.Enums;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,53 +13,81 @@ namespace BlazorBootstrapComponents.Components;
 
 public partial class BSTextArea
 {
-	protected override void OnInitialized()
-	{
-		if (Id == null || Id == string.Empty) Id = Guid.NewGuid().ToString();
-		if (FeedbackInValid.Length > 0 || FeedbackValid.Length > 0) FeedbackId = Guid.NewGuid().ToString();
-		base.OnInitialized();
-	}
-[Parameter] public string Class { get; set; } = string.Empty;
-	private string _Value;
+    protected override void OnInitialized()
+    {
+        _fieldIdentifier = FieldIdentifier.Create(ValueExpression);
+        if (Id == null || Id == string.Empty) Id = Guid.NewGuid().ToString();
 
-	[Parameter]
-	public string Value
-	{
-		get => _Value;
-		set
-		{
-			if (_Value == value) return;
-			_Value = value;
-			ValueChanged.InvokeAsync(value);
-		}
-	}
-	[Parameter] public EventCallback<string> ValueChanged { get; set; }
-	[Parameter] public BackgroundColorEnum Background { get; set; } = BackgroundColorEnum.Empty;
-	[Parameter] public ControlSizeEnum Size { get; set; } = ControlSizeEnum.Standard;
-	[Parameter] public string Title { get; set; }
-	[Parameter] public string Id { get; set; }
-	[Parameter] public bool FloatingLabel { get; set; }
-	[Parameter] public string PlaceHolder { get; set; }
-	[Parameter] public bool AutoComplete { get; set; }
-	[Parameter] public bool Disabled { get; set; }
-	[Parameter] public bool ReadOnly { get; set; }
-	[Parameter] public bool Required { get; set; }
-	[Parameter] public string FeedbackValid { get; set; } = string.Empty;
-	[Parameter] public string FeedbackInValid { get; set; } = string.Empty;
+        if (Id == null || Id == string.Empty) Id = Guid.NewGuid().ToString();
+        if (FeedbackInValid.Length > 0 || FeedbackValid.Length > 0) FeedbackId = Guid.NewGuid().ToString();
+        base.OnInitialized();
+    }
 
-	[Parameter] public int Rows { get; set; } = 3;
-	[Parameter] public bool HorizontalLayout { get; set; }
-	[Parameter] public int[] MediaWidthLabel { get; set; } = new int[] { -1, -1, -1, -1, -1, -1 };
-	[Parameter] public int[] MediaWidthTextBox { get; set; } = new int[] { -1, -1, -1, -1, -1, -1 };
-	[Parameter] public TextAlignmentEnum[] MediaLabelAlignment { get; set; } = new TextAlignmentEnum[] { TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty };
+    private FieldIdentifier _fieldIdentifier;
+    private string _fieldCssClasses => CascadedEditContext?.FieldCssClass(_fieldIdentifier) ?? "";
 
-	public string FeedbackId { get; set; } = string.Empty;
-	private Dictionary<string, object> AdditionalAttributes { get; set; } = new Dictionary<string, object>();
+    [CascadingParameter] private EditContext CascadedEditContext { get; set; }
 
-	private async void ValueChangedHandler(ChangeEventArgs args)
-	{
-		Value = args.Value as string;
-		await ValueChanged.InvokeAsync(Value);
-	}
+    [Parameter] public string Class { get; set; } = string.Empty;
+    private string _Value;
+
+    [Parameter]
+    public string Value
+    {
+        get => _Value;
+        set
+        {
+            if (_Value == value) return;
+            _Value = value;
+            ValueChanged.InvokeAsync(value);
+        }
+    }
+    [Parameter] public EventCallback<string> ValueChanged { get; set; }
+    [Parameter] public Expression<Func<string>> ValueExpression { get; set; }
+    [Parameter] public EventCallback<string> OnChange { get; set; }
+
+    [Parameter] public BackgroundColorEnum Background { get; set; } = BackgroundColorEnum.Empty;
+    [Parameter] public ControlSizeEnum Size { get; set; } = ControlSizeEnum.Standard;
+    [Parameter] public string Title { get; set; }
+    [Parameter] public string Id { get; set; }
+    [Parameter] public bool FloatingLabel { get; set; }
+    [Parameter] public string PlaceHolder { get; set; }
+    [Parameter] public bool AutoComplete { get; set; }
+    [Parameter] public bool Disabled { get; set; }
+    [Parameter] public bool ReadOnly { get; set; }
+    [Parameter] public bool Required { get; set; }
+    [Parameter] public string FeedbackValid { get; set; } = string.Empty;
+    [Parameter] public string FeedbackInValid { get; set; } = string.Empty;
+
+    [Parameter] public int Rows { get; set; } = 3;
+    [Parameter] public bool HorizontalLayout { get; set; }
+    [Parameter] public int[] MediaWidthLabel { get; set; } = new int[] { -1, -1, -1, -1, -1, -1 };
+    [Parameter] public int[] MediaWidthTextBox { get; set; } = new int[] { -1, -1, -1, -1, -1, -1 };
+    [Parameter] public TextAlignmentEnum[] MediaLabelAlignment { get; set; } = new TextAlignmentEnum[] { TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty };
+
+    public string FeedbackId { get; set; } = string.Empty;
+    private Dictionary<string, object> AdditionalAttributes { get; set; } = new Dictionary<string, object>();
+
+    private async void OnChangeHandler(ChangeEventArgs args)
+    {
+        string input = (string)args.Value;
+        Type typeNullable = typeof(string);
+
+            if (input.Length == 0)
+            {
+                Value = default(string);
+                await ValueChanged.InvokeAsync(default(string));
+                await OnChange.InvokeAsync(default(string));
+                CascadedEditContext?.NotifyFieldChanged(_fieldIdentifier);
+            }
+            else
+            {
+                Type typeValue = Nullable.GetUnderlyingType(typeNullable);
+                string output = input;
+                await ValueChanged.InvokeAsync(output);
+                await OnChange.InvokeAsync(output);
+                CascadedEditContext?.NotifyFieldChanged(_fieldIdentifier);
+            }
+    }
 
 }
--- Components/BSTextBox.razor.cs
+++ Components/BSTextBox.razor.cs
@@ -1,5 +1,7 @@
-﻿using BlazorBootstrapComponents.Enums;
+﻿using BaseClassLibrary.Classes;
+using BlazorBootstrapComponents.Enums;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,29 +15,29 @@ public partial class BSTextBox
 {
 	protected override void OnInitialized()
 	{
+		if(ValueExpression != null) _fieldIdentifier = FieldIdentifier.Create(ValueExpression);
 		if (Id == null || Id == string.Empty) Id = Guid.NewGuid().ToString();
 		base.OnInitialized();
 	}
 
-	private string _Value;
+	private FieldIdentifier _fieldIdentifier;
+	private string _fieldCssClasses => CascadedEditContext?.FieldCssClass(_fieldIdentifier) ?? "";
 
-	[Parameter] 
-	public string Value
-	{
-		get => _Value;
-		set
-		{
-			if(_Value== value) return;
-			_Value = value;
-			ValueChanged.InvokeAsync(value);
-		}
-	}
+	[CascadingParameter] private EditContext CascadedEditContext { get; set; }
+
+	[Parameter] public string Value { get; set; }
+	[Parameter] public EventCallback<string> ValueChanged { get; set; }
+	[Parameter] public Expression<Func<string>> ValueExpression { get; set; }
+
+	[Parameter] public EventCallback<string> OnChange { get; set; }
 
 	[Parameter] public string Class { get; set; } = string.Empty;
-	[Parameter] public EventCallback<string> ValueChanged { get; set; }
 	[Parameter] public BackgroundColorEnum Background { get; set; } = BackgroundColorEnum.Empty;
 	[Parameter] public ControlSizeEnum Size { get; set; } = ControlSizeEnum.Standard;
-	[Parameter] public string Title { get; set; } = string.Empty;
+    [Parameter] public bool Disabled { get; set; }
+    [Parameter] public bool ReadOnly { get; set; }
+    [Parameter] public bool Required { get; set; }
+    [Parameter] public string Title { get; set; } = string.Empty;
 	[Parameter] public string Id { get; set; }
 	[Parameter] public bool FloatingLabel { get; set; }
 	[Parameter] public string PlaceHolder { get; set; } = string.Empty;
@@ -45,7 +47,26 @@ public partial class BSTextBox
 	[Parameter] public int[] MediaWidthTextBox { get; set; } = new int[] { -1, -1, -1, -1, -1, -1 };
 	[Parameter] public TextAlignmentEnum[] MediaLabelAlignment { get; set; } = new TextAlignmentEnum[] { TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty, TextAlignmentEnum.Empty };
 
-	public string FeedbackId { get; set; } = string.Empty;
 	private Dictionary<string, object> AdditionalAttributes { get; set; } = new Dictionary<string, object>();
+
+	private async Task OnChangeHandler(ChangeEventArgs args)
+	{
+		string input = (string)args.Value;
+
+			if (input.Length == 0)
+			{
+				Value = default;
+				await ValueChanged.InvokeAsync(default);
+				await OnChange.InvokeAsync(default);
+				CascadedEditContext?.NotifyFieldChanged(_fieldIdentifier);
+			}
+			else
+			{
+				string output = input;
+				await ValueChanged.InvokeAsync(output);
+				await OnChange.InvokeAsync(output);
+				CascadedEditContext?.NotifyFieldChanged(_fieldIdentifier);
+			}
+	}
 
 }
