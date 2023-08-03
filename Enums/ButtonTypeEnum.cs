1ff7acac9fa90f31f1c49e5c64ff93aa5ee07b81
ImperialInd <31216511+ImperialInd@users.noreply.github.com>
2023-08-03 07:40:28 -0500

    Update

---
+++ Enums/ButtonTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBootstrapComponents.Enums;
+
+public enum ButtonTypeEnum
+{
+    Button,
+    Submit,
+    Reset
+}
