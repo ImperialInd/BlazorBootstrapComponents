84d36a8d2d57ebed1a79084bdeae5f980cbd79e2
ImperialInd <31216511+ImperialInd@users.noreply.github.com>
2023-09-15 08:30:55 -0500

    Update

---
+++ Enums/BorderRoundedSideEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorBootstrapComponents.Enums;
+
+public enum BorderRoundedSideEnum
+{
+    None = 0,
+    All = 1,
+    Top = 2,
+    End = 3,
+    Bottom = 4,
+    Start = 5,
+    Circle = 6,
+    Pill = 7
+}
